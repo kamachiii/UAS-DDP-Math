9362e3f375d324732edf8b9b3cfa7064ed305908
kamachiii <hengkert3jy@gmail.com>
2025-01-08 11:27:09 +0700

    update: remove contoh & structur folder adds

--- math_project/urls.py
+++ math_project/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from calculator.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #* Path Admin
+
+    path('', index, name="index"),
 ]
